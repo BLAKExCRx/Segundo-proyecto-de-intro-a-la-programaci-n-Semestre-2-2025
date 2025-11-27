9e5be5dfd4e28d20ea23b8beef671a2db164ba20
BLAKExCRx <orlandocalderonmendez@gmail.com>
2025-11-26 19:04:50 -0600

    actualizacion del menu.py
    se codifican las posiciones ,acho,altura,color, tamaño de bordes de todo el menu principal, despues añadire los comentarios

--- menu.py
+++ menu.py
@@ -1,16 +1,57 @@
 #archivo de menu.py
 #primero los import's
-#por ahora no funciona nada por estar incompleto
+#por ahora no funciona nada por estar incompleto y los comentarios se actualizan a futuro
 import pygame 
 import sys
 from puntuacion import Puntuacion #se importa de puntuacion la tabla de puntacion
 from config import ANCHO, ALTO, FPS, COLORES # se usa para importar valores como la altura,ancho colores y mas
 class Menu:
     def __init__(self, screen, puntuacion): #definicion de la pantalla principal del menu 
-      pass
+        self.screen = screen
+        self.puntuacion = puntuacion
+        self.clock = pygame.time.Clock()
+        self.font_title = pygame.font.SysFont('arial', 50, bold=True)
+        self.font_subtitle = pygame.font.SysFont('arial', 24, bold=True)
+        self.font_button = pygame.font.SysFont('arial', 24, bold=True)
+        self.font_small = pygame.font.SysFont('arial', 18)
+        self.colors = COLORES
+
+         # Fondo
+        try:
+            self.background = pygame.image.load('fondo_menu.png')
+            self.background = pygame.transform.scale(self.background, (ANCHO, ALTO))
+        except pygame.error:
+            self.background = None
+        
+        # Estados
+        self.state = 'name_input'
+        self.nombre = ''
+        self.cursor_visible = True
+        self.cursor_timer = 0
+        self.modo_seleccionado = None
+        self.dificultad = 'medio'
+        
+        self.rects = self._definir_rects()
+        
+        # Luna
+        self.moon_pos = (ANCHO // 2 - 300, 50)
+        self.moon_radius = 80
         
     def _definir_rects(self):#definicion de las posciones de las opciones del menu 
-      pass
+        rects = {}
+        rects['input_nombre'] = pygame.Rect(ANCHO - 400, 50, 350, 40)
+        rects['panel_modos'] = pygame.Rect(50, 200, 300, 250)
+        rects['modo_escapa'] = pygame.Rect(55, 210, 290, 50)
+        rects['modo_cazador'] = pygame.Rect(55, 270, 290, 50)
+        rects['salir'] = pygame.Rect(55, 390, 290, 50)
+        
+        rects['dificil_facil'] = pygame.Rect(ANCHO // 2 - 75, 150, 150, 50)
+        rects['dificil_medio'] = pygame.Rect(ANCHO // 2 + 75, 150, 150, 50)
+        rects['dificil_dificil'] = pygame.Rect(ANCHO // 2 + 225, 150, 150, 50)
+        
+        rects['top_escapa'] = pygame.Rect(ANCHO // 2 - 75, 250, 150, 150)
+        rects['top_cazador'] = pygame.Rect(ANCHO // 2 + 125, 250, 150, 150)
+        return rects
     
     def handle_events(self, events): #definicion para los eventos o acciones segun el menu
         pass
